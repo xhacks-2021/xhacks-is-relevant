ddab7fbe30bfef03bb7e16ea2eec949c135d28dc
Max Huang <letmaxknow@gmail.com>
2021-08-07 09:10:17 -0400

    init commit
    Co-Authored-By: Emily Yu <35238826+emily-emily@users.noreply.github.com>

---
+++ routes/isRelevant.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const language = require('@google-cloud/language');
+let router = express.Router();
+
+router.post('/', async (req, res, next) => {
+
+  let { questions, text } = req.body;
+
+  let relevances = await getRelevances(questions, text);
+  res.json({ length: relevances.length, relevances });
+});
+
+// input: list of question-answer pairs
+// output: list of question-answer-relevance tuples
+const getRelevances = async (questionAnswerList, text) => {
+
+  let keywords = (await getKeywords(text));
+
+  let relevances = new Promise((resolve) => {
+    let ret = questionAnswerList.map(({ answer, question }) => {
+      return { question, answer, relevance: relevance(question, answer, keywords) };
+    });
+    ret.sort((a, b) => b.relevance - a.relevance); // sort by relevance (descending)
+    resolve(ret);
+  });
+  
+  return (await relevances);
+} 
+
+const getKeywords = async (text) => {
+
+  const client = new language.LanguageServiceClient();
+  const document = {
+    content: text,
+    type: 'PLAIN_TEXT',
+  };
+  const [result] = await client.analyzeEntities({ document });
+
+  // Convert keywords to lowercase.
+  // Combine duplicate keywords.
+  // Return as object with names as keys. 
+  return result.entities.reduce((acc, { name, salience }) => {
+    acc[name.toLowerCase()] = {
+      salience: (acc.hasOwnProperty(name) ? acc[name].salience : 0) + salience
+    };
+    return acc;
+  }, {});
+}
+
+// input: question, answer, and list of keywords
+// output: relevance rating for question and answer
+//    --> based on number of keywords in question AND answer
+const relevance = (question, answer, keywords) => {
+
+  const strength = (words) => {
+    return Object.keys(keywords).reduce((acc, phrase) => {
+      let includesPhrase = phrase.split(' ').reduce((acc, word) => acc || words.includes(word), false);  // Check if any keyword in phrase matches words.
+      return acc + (includesPhrase ? keywords[phrase].salience : 0);
+    }, 0);
+  }
+
+  const punctuationRegex = new RegExp(/[;:,.?!\/"#\s]+/);
+  let words = (question + '#' + answer).toLowerCase().split(punctuationRegex);  // Split by punctuation and spaces.
+  return strength(words);
+}
+
+// TODO?: return top x% of questions to client
+
+module.exports = router;
---
+++ run.bat
@@ -0,0 +1,2 @@
+set GOOGLE_APPLICATION_CREDENTIALS=D:\Programming Projects\is-relevant\gsa-credentials.json
+npm start
