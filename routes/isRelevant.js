e93e079530c6652e5b479b46f9c1796fa13cc0c3
Max Huang <letmaxknow@gmail.com>
2021-08-07 17:41:03 -0400

    Update isRelevant.js

--- routes/isRelevant.js
+++ routes/isRelevant.js
@@ -2,6 +2,12 @@ const express = require('express');
 const language = require('@google-cloud/language');
 let router = express.Router();
 
+router.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 router.post('/', async (req, res, next) => {
 
   let { questions, text } = req.body;
